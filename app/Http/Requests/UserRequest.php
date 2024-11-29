a5f6fc56946bd48bfd8a22a5269fdea538ee3c06
Aglae Duin <aglaef.duing@autonoma.co>
2024-11-28 21:12:02 -0500

    Se agregó UserRequest para el método crear

---
+++ app/Http/Requests/UserRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use App\Models\User;
+class UserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            //
+            'nombre_completo' => ['required', 'string', 'max:255'],
+            'username' => ['required', 'string', 'max:255', 'unique:'.User::class],
+            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
+            'rol' => ['required', 'string', 'max:255'],
+            'password' => ['required', 'confirmed'],
+        ];
+    }
+}
